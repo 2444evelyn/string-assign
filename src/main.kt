d72021461ea38b788e6c83d126fdd4a70360668b
@2444evelyn <everlinetsuma@gmail.com>
2022-03-01 22:43:40 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    var text = "akirachix"
+    var u = text[0].toString() + text[2] + text[3]
+    println(u)
+
+    var x = "Tsuma"
+    var y = 20
+    var detail = "Hi,my name is $x and i am $y years old"
+    println(detail)
+
+    var town = "Mombasa"
+    println(town.length)
+
+    var Name = "evelyn"
+    if(Name== "bleh"){
+        println("thats me")
+    }else{println("thats not me")
+    }
+}
